da7fb8bf5503eac5168198c68b155bd7a3ec84f4
Donat <kiss.and.donat@gmail.com>
2016-05-18 01:58:54 +0200

    gmt+1 server time

--- applications/rapidpoll/src/main/java/com/appsball/rapidpoll/commons/utils/DateStringFormatter.java
+++ applications/rapidpoll/src/main/java/com/appsball/rapidpoll/commons/utils/DateStringFormatter.java
@@ -11,6 +11,8 @@ import org.joda.time.Period;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
+import java.util.TimeZone;
+
 public class DateStringFormatter {
 
     private Resources resources;
@@ -37,7 +39,8 @@ public class DateStringFormatter {
             return 0;
         }
         //2015-11-06 13:27:00
-        DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss").withZone(DateTimeZone.forOffsetHours(0));
+
+        DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss").withZone(DateTimeZone.forTimeZone(TimeZone.getTimeZone("GMT+1")));
         DateTime date = formatter.parseDateTime(publishDate);
         Period diff = new Period(date, toCompare);
         int hour = diff.getHours();
